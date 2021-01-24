2ae9d83075fd33462daa0e59c66bdcc98af559ae
koba-masa <kobayashi_mshr@koba-masa.com>
2021-01-24 16:14:37 +0900

    半角スペースを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_many :microposts
 
     validates :name, presence: true
-    validates :email, presence: true, format:  {with: /\A[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}\z/}
+    validates :email, presence: true, format: {with: /\A[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}\z/}
 end

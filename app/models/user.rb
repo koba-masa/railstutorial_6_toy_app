b07902233baff1f9bff92cd94912b599edf5d440
koba-masa <kobayashi_mshr@koba-masa.com>
2021-01-24 16:13:32 +0900

    Userモデルにメールアドレスのパターンチェックを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_many :microposts
 
     validates :name, presence: true
-    validates :mail, presence: true
+    validates :email, presence: true, format:  {with: /\A[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}\z/}
 end
